5db7f6f348e4aa2487f2626f87f675ca3e62b386
Eli <elimaynard923@gmail.com>
2020-01-13 01:46:19 -0500

    More examples

--- example.py
+++ example.py
@@ -7,13 +7,35 @@ poem = unplate.template(
   # all alone...
 )
 
-print(f"A short poem:{poem}")
+print(f"= A short poem =\n{poem}")
 
 
-serious = unplate.template(
-  # Australia is on fire.
-  # Global climate change is out of control.
-  # We may not make it.
+# unplate handles backslahses nicely, no escapes needed
+backslashes = unplate.template(
+  # Here's a backslash for you: \
+  # \ this line begins with a backslash
+  # this line ends with a backslash \
+  # this line has \ a backslash in the middle!
 )
+print(f'\n= Backslashes =\n{backslashes}')
 
-print(f"\nAnd now for something more serious:\n{serious}")
+
+# escape the interpolation characters with \:
+escapes = unplate.template(
+  # This: {2+2} will be interpolated
+  # This: \{2+2\} will not
+)
+print(f'\n= Escapes =\n{escapes}')
+
+
+# you can also change the escape character as
+# well as the interpolation characters
+unplate.options.interpolation_left = '<'
+unplate.options.interpolation_right = '>'
+unplate.options.escape_char = '~'
+options = unplate.template(
+  # This: <1/2> will be interpolated
+  # This: {1/2} no longer will be
+  # This: ~<1/2~> will be escaped
+)
+print(f'\n= Options =\n{options}')
